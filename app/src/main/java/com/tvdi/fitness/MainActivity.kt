a7bcf9f5763c641dc64372c91b5b960aa400ae42
Samuel Chan <p399975@gmail.com>
2021-03-08 16:03:42 +0800

    fitness: empty project scaffold for kotlin

---
+++ app/src/main/java/com/tvdi/fitness/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tvdi.fitness
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
